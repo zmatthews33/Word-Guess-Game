9a9578dd0594de543d627fce2f63a951eb2ec8f6
whatevermane <zmatt33@gmail.com>
2019-02-08 19:24:39 -0600

    Laid out HTML and CSS with bootstrap, added custom CSS and added variables in JS

---
+++ assets/javascript/game.js
@@ -0,0 +1,23 @@
+var ["Jimi Hendrix Experience", "The Who", "Led Zeppelin", 
+"Pink Floyd", "The Beatles", "The Rolling Stones", 
+"The Doors","The Beach Boys", "Grateful Dead", 
+"Jefferson Airplane", "The Animals"]
+
+var word = words[Math.floor(Math.random() * words.length)];
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* for audio?
+var audio = new Audio('#.mp3');
+audio.play();
+*/
