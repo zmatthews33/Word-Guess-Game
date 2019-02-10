7f88db4c640e06fc94f6fc16c6dcac18faa0ec55
whatevermane <zmatt33@gmail.com>
2019-02-10 07:57:21 -0600

    made substantial progress. Game is working but need to fix spaces issue in guess word

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,23 +1,138 @@
-var ["Jimi Hendrix Experience", "The Who", "Led Zeppelin", 
-"Pink Floyd", "The Beatles", "The Rolling Stones", 
-"The Doors","The Beach Boys", "Grateful Dead", 
-"Jefferson Airplane", "The Animals"]
+// Bands
 
-var word = words[Math.floor(Math.random() * words.length)];
+var bands = [
+  /* "JIMI$HENDRIX$EXPERIENCE",
+  "THE$WHO",
+  "LED$ZEPPELIN",
+  "PINK$FLOYD",
+  "THE$BEATLES",
+  "THE$ROLLING$STONES",
+  "THE$DOORS",
+  "THE$BEACH$BOYS",
+  "GRATEFUL$DEAD",
+  "JEFFERSON$AIRPLANE",
+  "THE$ANIMALS",*/
+  "CREAM"
+];
 
+var numbOfTries = 10;
 
+var lettersGuessed = [];
+var currentWordIndex;
+var guessingWord = [];
+var guessesRemaining = 0;
+var gameOver = false;
+var wins = 0;
 
+var winSound = new Audio("./assets/sounds/...");
+var loseSound = new Audio("./assets/sounds/losetrombone.wav");
 
+function resetGame() {
+  //resets game
+  guessesRemaining = numbOfTries;
 
+  function bandsNoSpaces() {
+    var bands = document.getElementById("currentWord").innerHTML;
+    var bandsNoSpaces = bands.replace("$", "");
+    document.getElementById("currentWord").innerHTML = bandsNoSpaces;
+  }
 
+  bandsNoSpaces();
 
+  currentWordIndex = Math.floor(Math.random() * bands.length);
 
+  lettersGuessed = [];
+  guessingWord = [];
 
+  document.getElementById("hangmanImage").src = "assets/images/0.png";
 
+  for (var i = 0; i < bands[currentWordIndex].length; i++) {
+    guessingWord.push("_");
+  }
 
+  document.getElementById("pressKeyTryAgain").style.cssText = "display: none";
+  document.getElementById("gameover").style.cssText = "display: none";
+  document.getElementById("youwin").style.cssText = "display: none";
 
+  updateDisplay();
+}
 
-/* for audio?
-var audio = new Audio('#.mp3');
-audio.play();
-*/
+function updateDisplay() {
+  document.getElementById("winsTotal").innerText = wins;
+
+  var guessingWordText = " ";
+  for (var i = 0; i < guessingWord.length; i++) {
+    guessingWordText += guessingWord[i];
+  }
+
+  document.getElementById("currentWord").innerText = guessingWordText;
+  document.getElementById("guessesRemaining").innerText = guessesRemaining;
+  document.getElementById("lettersGuessed").innerText = lettersGuessed;
+}
+
+function updateHangmanImage() {
+  document.getElementById("hangmanImage").src =
+    "assets/images/" + (numbOfTries - guessesRemaining) + ".png";
+}
+
+function evaluateGuess(letter) {
+  var positions = [];
+
+  for (var i = 0; i < bands[currentWordIndex].length; i++) {
+    if (bands[currentWordIndex][i] === letter) {
+      positions.push(i);
+    }
+  }
+
+  if (positions.length <= 0) {
+    guessesRemaining--;
+    updateHangmanImage();
+  } else {
+    for (var i = 0; i < positions.length; i++) {
+      guessingWord[positions[i]] = letter;
+    }
+  }
+}
+
+function checkWin() {
+  if (guessingWord.indexOf("_") === -1) {
+    document.getElementById("youwin").style.cssText = "display: block";
+    document.getElementById("pressKeyTryAgain").style.cssText =
+      "display: block";
+    wins++;
+    winSound.play();
+    gameOver = true;
+  }
+}
+
+function checkLoss() {
+  if (guessesRemaining <= 0) {
+    loseSound.play();
+    document.getElementById("gameover").style.cssText = "display: block";
+    document.getElementById("pressKeyTryAgain").style.cssText = "display:block";
+    gameOver = true;
+  }
+}
+
+function playerGuess(letter) {
+  if (guessesRemaining > 0) {
+    if (lettersGuessed.indexOf(letter) === -1) {
+      lettersGuessed.push(letter);
+      evaluateGuess(letter);
+    }
+  }
+}
+
+document.onkeydown = function(event) {
+  if (gameOver) {
+    resetGame();
+    gameOver = false;
+  } else {
+    if (event.keyCode >= 65 && event.keyCode <= 90) {
+      playerGuess(event.key.toUpperCase());
+      updateDisplay();
+      checkWin();
+      checkLoss();
+    }
+  }
+};
